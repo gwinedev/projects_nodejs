aa3fe9c4b33b61157bd18736c98dfe85e8e0ee4d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-05-06 17:52:21 +0100

    app.js updated

--- e-commerce-api/app.js
+++ e-commerce-api/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("./config/database");
 const productRoutes = require("./routes/products");
 const authRoutes = require("./routes/auth");
+const reviewRoutes = require("./routes/review");
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/reviews", reviewRoutes);
 
 const PORT = process.env.PORT || 5000;
 
