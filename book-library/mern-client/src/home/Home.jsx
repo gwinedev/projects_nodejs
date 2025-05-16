09b5393c2f2de74a29b85ad85902d435396fe778
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-05-16 22:38:50 +0100

    created componenets

--- book-library/mern-client/src/home/Home.jsx
+++ book-library/mern-client/src/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = () => {
+const home = () => {
   return <div>Hello, World!</div>;
 };
 
-export default Home;
+export default home;
--- book-library/mern-client/src/routers/router.jsx
+++ book-library/mern-client/src/routers/router.jsx
@@ -1,15 +1,32 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../home/home";
+import App from "../App";
+import Shop from "../shop/Shop";
+import About from "../components/About";
+import Blog from "../components/Blog";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
-        {
-            path:'/',
-            element:
-        }
-    ]
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
   },
 ]);
 
