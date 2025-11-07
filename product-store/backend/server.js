1e00f2935f9b00cfe899bad5d620bfd7af0458a7
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-07 19:51:01 +0100

    create a post route for product

--- product-store/backend/server.js
+++ product-store/backend/server.js
@@ -1,12 +1,36 @@
 // Entry point for our API
 // You can make express type a module so you can import instead of require it
 import express from "express";
-import { connectDB } from './config/db.js'
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({ message: "Server is ready" });
+});
+
+app.post("/api/products", async (req, res) => {
+  //   console.log(req.body);
+
+  const product = req.body;
+  //   console.log(product.price);
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error(("Error in creating product: ", error.message));
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 const PORT = 5000;
