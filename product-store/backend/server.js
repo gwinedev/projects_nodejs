4933aecbbbefc0d8ab672283c9b91d871067bf27
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-07 18:20:05 +0100

    connect to external database

--- product-store/backend/server.js
+++ product-store/backend/server.js
@@ -1,12 +1,18 @@
 // Entry point for our API
 // You can make express type a module so you can import instead of require it
+import express from "express";
+import { connectDB } from './config/db.js'
 
-import express from 'express';
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send({ message: "Server is ready" });
+});
 
+const PORT = 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started on port ${PORT}...`);
+});
 
-app.listen(5000, () =>{
-    console.log("Server started...");
-    
-})
+//mongodb+srv://gwinedev_db_user:qUFddZhDAXP9YBOf@cluster0.j1ajq1d.mongodb.net/?appName=Cluster0
