b9dd0293a0528f31a81a9b29f1d11ebabeeeb83f
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-08 14:18:35 +0100

    implement update and delete methods

--- product-store/backend/server.js
+++ product-store/backend/server.js
@@ -3,12 +3,19 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({ message: "Server is ready" });
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(201).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error in fetching data", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.post("/api/products", async (req, res) => {
@@ -33,6 +40,43 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.error("Error updatung product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: `Product with id: ${id} deleted` });
+  } catch (error) {
+    console.error("Error in deleting product", error.message);
+
+    res
+      .status(404)
+      .json({ success: false, message: `Product with id: ${id} not found` });
+  }
+});
 const PORT = 5000;
 app.listen(PORT, () => {
   connectDB();
