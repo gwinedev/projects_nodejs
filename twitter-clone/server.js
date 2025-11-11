237796e5864b8e5746ffa9130b77536aa70762b9
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-11 19:21:53 +0100

    made a getMe function to get user's profile

--- twitter-clone/server.js
+++ twitter-clone/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config({ quiet: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 8000;
