3a6730050c62f0e850ccbf1e005c886abdbf9d5f
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-18 16:35:03 +0100

    implement follow and unfollow controller

--- messaging-app/backend/controllers/user.controller.js
+++ messaging-app/backend/controllers/user.controller.js
@@ -11,3 +11,27 @@ export const getUserProfile = async (req, res) => {
   res.status(200).json({ user });
 };
 
+export const followUnfollowUser = async (req, res) => {
+  const { id } = req.params; //id of who to follow or unfollow
+  const userToModify = await User.findbyId(id);
+  const currentUser = await User.findbyId(req.user._id);
+
+  if (id === currentUser._id.toString()) {
+    return res
+      .status(400)
+      .json({ error: "You cannot follow or unfollow yourself" });
+  }
+  if (!userToModify) return res.status(404).json({ error: "User not found" });
+  if (!currentUser) return res.status(404).json({ error: "User not found" });
+
+  const isFollowing = currentUser.following.includes(id);
+  if (isFollowing) {
+    // Unfollow current user by 1. Pull currentUSer id from other user and vise versa
+    await User.findByIdAndUpdate(id, {
+      $pull: { following: currentUser._id },
+    });
+    await User.findByIdAndUpdate(currentUser._id, {
+      $pull: { following: id },
+    });
+  }
+};
--- messaging-app/backend/routes/user.route.js
+++ messaging-app/backend/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getUserProfile } from "../controllers/user.controller.js";
+import {
+  getUserProfile,
+  followUnfollowUser,
+} from "../controllers/user.controller.js";
 import asyncHandler from "../lib/utils/asyncHandler.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, asyncHandler(getUserProfile));
+router.get("/follow/:id", protectRoute, asyncHandler(followUnfollowUser));
 
 export default router;
