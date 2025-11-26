ab6c79998f68ad579d4e4ea47fd7d02355d556ba
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-26 21:56:58 +0100

    update: controllers and models updated

--- chat-app-yt/backend/controllers/auth.controller.js
+++ chat-app-yt/backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signupUser = async (req, res) => {
   try {
@@ -13,7 +14,7 @@ export const signupUser = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
     // Hash password
-    const salt = bcrypt.getSalt(10);
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -27,6 +28,7 @@ export const signupUser = async (req, res) => {
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
     if (newUser) {
+      await generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(200).json({
@@ -44,9 +46,37 @@ export const signupUser = async (req, res) => {
   }
 };
 
-export const loginUser = (req, res) => {
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect)
+      return res.status(404).json({ error: `Invalid username or password` });
+
+    generateTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
   console.log("Login");
 };
-export const logoutUser = (req, res) => {
-  console.log("Log out");
+export const logoutUser = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
