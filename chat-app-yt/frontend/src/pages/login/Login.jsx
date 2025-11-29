02439b8c7245bcbcb0cf3558a5c0dbc78ddddd1b
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-29 16:47:49 +0100

    frontend setup

---
+++ chat-app-yt/frontend/src/pages/login/Login.jsx
@@ -0,0 +1,11 @@
+const Login = () =>{
+    return(
+        <div className="flex flex-col items-center justify- min-w-96 mx-auto">
+            <div className="w-full p-6 rounded-lg shadow-md bg-white/10 border border-white/20">
+                <h1 className="text-3xl font-bold text-center text-gray-300">Login</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
